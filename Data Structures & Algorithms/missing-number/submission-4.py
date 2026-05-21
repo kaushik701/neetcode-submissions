985a9643d3383d4cf1166a5229d482f5df19a6ea
Kaushik Iyer <54633514+kaushik701@users.noreply.github.com>
2026-05-21 16:16:14 -0700

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        """
+        expected = len(nums) * (len(nums)+1) // 2
+        actual = sum(nums)
+        return expected - actual
+        """
+        """xorr = 0
+        for i in range(len(nums)+1):
+            xorr ^= i
+        for num in nums: xorr ^= num
+        return xorr
+        """
+        res = len(nums)
+        for i in range(len(nums)):
+            res += (i-nums[i])
+        return res
+        
